06a54dfdf3d94a3c606a82ec4f938853ffa47c24
Kirill Gusev <myaumuraa@gmail.com>
2024-02-15 08:27:27 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Minesweeper
+//
+//  Created by Kirill Gusev on 15.02.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
